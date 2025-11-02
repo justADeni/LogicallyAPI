e54354600f225a5ca3245523eb11cc4ae9d9bf1d
justADeni <deninikdo@gmail.com>
2025-11-03 00:49:18 +0100

    Squashed commit of the following:
    commit 0ec72d5e7538e27bb2c54c9de3741702153dbb2b
    Author: justADeni <deninikdo@gmail.com>
    Date:   Mon Nov 3 00:48:15 2025 +0100
    
        feat: added javadoc
    
    commit cdcaaeee0dddc03e13c44a9cd7dc674f217dedcd
    Author: justADeni <deninikdo@gmail.com>
    Date:   Mon Nov 3 00:31:55 2025 +0100
    
        feat: added javadoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,16 +23,16 @@ dependencies {
     paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
 }
 
-tasks {
-    compileJava {
-        options.release.set(javaVersion)
-    }
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             artifact(tasks.shadowJar)
+            artifact(tasks.named("javadocJar"))
         }
     }
 }
